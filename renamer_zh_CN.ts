7b9373e8b0fd6f25cc85979e41569d4c644e5cca
Redtux <redtux@RedtuxdeMacBook-Air.local>
2026-07-15 16:09:07 +0800

    Chinese translation

--- renamer_zh_CN.ts
+++ renamer_zh_CN.ts
@@ -1,3 +1,68 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_CN"></TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainForm</name>
+    <message>
+        <source>Find:</source>
+        <translation>查找：</translation>
+    </message>
+    <message>
+        <source>Replace:</source>
+        <translation>替换：</translation>
+    </message>
+    <message>
+        <source>Case Sensitive</source>
+        <translation>区分大小写</translation>
+    </message>
+    <message>
+        <source>Regular Expression</source>
+        <translation>正则表达式</translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>重命名</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Enable dark theme</source>
+        <translation>暗主题</translation>
+    </message>
+    <message>
+        <source>Border color</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Select window border color</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Settings(&amp;S)</source>
+        <translation>设置（&amp;S）</translation>
+    </message>
+</context>
+<context>
+    <name>TableModel</name>
+    <message>
+        <source>Current Name</source>
+        <translation>当前名称</translation>
+    </message>
+    <message>
+        <source>New Name</source>
+        <translation>拟更改名称</translation>
+    </message>
+</context>
+<context>
+    <name>TableView</name>
+    <message>
+        <source>Empty List</source>
+        <translation>清空列表</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>移除选择的项</translation>
+    </message>
+</context>
+</TS>
